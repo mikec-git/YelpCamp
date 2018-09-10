dc692fd4b50a5b0704cc92ed99638d71d02bce01
Mike C <mike.choi@live.ca>
2018-09-10 22:16:22 +0000

    Added middleware to check if a user is currently logged in, and refactored navbar to only display login/register when user is not logged in

--- V6/app.js
+++ V6/app.js
@@ -29,6 +29,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Custom Middleware
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // ==================== //
 //        ROUTES        //
