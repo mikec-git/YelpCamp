6d922abb1fa608678a43215a8692930159b80a29
Mike C <mike.choi@live.ca>
2018-09-10 22:44:36 +0000

    Refactored code: moved routes into respective file in route directory; used express.Router()

---
+++ V7/app.js
@@ -0,0 +1,51 @@
+var express         = require("express"),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
+    
+var app             = express();
+
+// Requiring routes
+var campgroundRoutes    = require("./routes/campgrounds"),
+    commentRoutes       = require("./routes/comments"),
+    authRoutes          = require("./routes/index");
+
+mongoose.connect("mongodb://localhost/yelp_camp_v7", {useNewUrlParser: true}); // Connect to mongoDB
+app.use(bodyParser.urlencoded({extended: true})); //extended: true allows for any object type rather than just string/arrays
+app.set("view engine", "ejs");
+seedDB();
+
+// PASSPORT CONFIG
+app.use(express.static(__dirname + "/public"));
+app.use(require("express-session")({
+    secret: "Blue eyes white dragon",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Custom Middleware
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+//Using routes
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/", authRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp server has started!");
+});
